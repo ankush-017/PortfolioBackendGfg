06848054d0d5b061dbf63cba6dbb8d82a1b4a454
Ankush kumar <ankushkumar1412005@gmail.com>
2025-04-01 00:41:42 +0530

    Describe your changes here

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const apicache = require('apicache');
-const axios = require('axios');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
-const cache = apicache.middleware;
+app.use(cors());
 
-// Apply caching to the GFG profile route with a duration of 5 minutes
-app.get('/gfg/:username', cache('5 minutes'), async (req, res) => {
-  const { username } = req.params;
-  const apiUrl = `https://geeks-for-geeks-api.vercel.app/${username}`;
+app.get("/gfg/:username", async (req, res) => {
+  const username = req.params.username;
+  const apiUrl = "https://geeks-for-geeks-api.vercel.app/${username}";
 
   try {
     const response = await axios.get(apiUrl);
     res.json(response.data);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch data' });
+    res.status(500).json({ error: "Failed to fetch data" });
   }
 });
 
-app.listen(8080, () => console.log('Server running on port 8080'));
+
+app.listen(8080, () => console.log("Server running on port 8080"));
